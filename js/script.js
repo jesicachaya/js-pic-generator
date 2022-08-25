20dc0bb2faf35f312907bea8b9e4a5cbaa700f3f
Jesica Chaya <jesicachaya@gmail.com>
2022-08-25 15:58:00 -0400

    Created functions that select a random image index, a random image, and then use that random image's property information to populate the webpage with an image and artist name when the button is clicked

--- js/script.js
+++ js/script.js
@@ -7,7 +7,21 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const results = await fetch("https://picsum.photos/v2/list?limit=100.");
     const images = await results.json();
-    console.log(images);
+    selectRandomImage(images);
 }
 
-getImage();
+const selectRandomImage = function(images) {
+    randomImageIndex = Math.floor(Math.random()*images.length);
+    randomImage = images[randomImageIndex];
+    updateInfo(randomImage);
+}
+
+const updateInfo = function(randomImage) {
+    authorSpan.innerText = randomImage.author;
+    img.src = randomImage.download_url;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function() {
+    getImage();
+})
